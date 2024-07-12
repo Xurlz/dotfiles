ab7b8b8958a3975b409e2f02f504c2fc763eca37
Charles Sena <charlessena@gokeitecnologia.com.br>
2024-07-12 15:01:36 -0300

    Configurações relacionadas ao `ranger_cd`

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -1,11 +1,27 @@
-function ranger_wrapper {
-  /usr/bin/env ranger $*
-  local quit_cd_wd_file="$HOME/.cache/ranger/quit_cd_wd"
-  if [ -s "$quit_cd_wd_file" ]; then
-    cd "$(cat $quit_cd_wd_file)"
-    true > "$quit_cd_wd_file"
-  fi
+# shellcheck shell=sh
+
+# Compatible with ranger 1.4.2 through 1.9.*
+#
+# Automatically change the current working directory after closing ranger
+#
+# This is a shell function to automatically change the current working
+# directory to the last visited one after ranger quits. Either put it into your
+# .zshrc/.bashrc/etc or source this file from your shell configuration.
+# To undo the effect of this function, you can type "cd -" to return to the
+# original directory.
+
+ranger_cd() {
+    temp_file="$(mktemp -t "ranger_cd.XXXXXXXXXX")"
+    ranger --choosedir="$temp_file" -- "${@:-$PWD}"
+    if chosen_dir="$(cat -- "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+        cd -- "$chosen_dir"
+    fi
+    rm -f -- "$temp_file"
 }
 
-alias ranger='ranger_wrapper'
+# This binds Ctrl-O to ranger_cd:
+bind '"\C-o":"ranger_cd\C-m"'
+
+export ranger_cd
+
 
